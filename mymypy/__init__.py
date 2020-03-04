c6eebc2480fc5b366979b3e5c67ac5fd738d1e83
seto-r <theshootingstar@hotmail.co.jp>
2020-03-04 10:33:12 +0900

    add setup.py, ci (flake8, black, mypy)

---
+++ mymypy/__init__.py
@@ -0,0 +1,6 @@
+__copyright__ = "Copyright (C) 2018 Ryunosuke Seto"
+__version__ = "0.1.0"
+__license__ = "MIT License"
+__author__ = "Seto Ryunosuke"
+__author_email__ = "theshootingstar@hotmail.co.jp"
+__url__ = "http://github.com/seto-r/mymypy"
